5396a3675e32336afbe6fea4e6b1838989d70b65
Mohan Yin <mohanyin@gmail.com>
2023-12-26 15:37:24 -0800

    proper scss usage

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,11 @@
 <script setup lang="ts">
-import { PhInfo, PhArrowRight } from '@phosphor-icons/vue'
+import IntroScreen from './components/IntroScreen.vue'
 </script>
 
 <template>
   <header>mohan yin</header>
 
   <main>
-    <div class="intro">
-      <div class="intro__text">
-        <h1>
-          <span class="intro__text__hi">Hi</span>
-          <span class="intro__text__name">I’m Mohan.</span>
-        </h1>
-        <button class="intro__info">
-          <PhInfo />
-        </button>
-      </div>
-
-      <button class="intro__cta">
-        Let’s talk
-        <div class="intro__cta__arrow">
-          <PhArrowRight />
-        </div>
-      </button>
-    </div>
+    <IntroScreen />
   </main>
 </template>
-
-<style lang="scss" scoped>
-@import '@/styles/variables.scss';
-@import '@/styles/helpers.scss';
-
-.intro {
-  @include center-contents;
-
-  flex-direction: column;
-  gap: 36px;
-  position: fixed;
-  inset: 0;
-}
-
-.intro__text {
-  position: relative;
-}
-
-.intro__text__hi {
-  @include circle(128px);
-
-  font-weight: $font-weight-medium;
-  font-size: $font-size-display-2;
-  background-color: $color-orange-500;
-  border: 2px solid $color-black;
-  display: block;
-  text-align: center;
-  line-height: 118px;
-  margin-bottom: -24px;
-  transform: rotate(-10deg);
-}
-
-.intro__text__name {
-  font-family: $font-family-mono;
-  font-weight: $font-weight-bold;
-  font-size: $font-size-display-1;
-  color: $color-white;
-  background-color: $color-black;
-  display: block;
-  text-align: center;
-  border-radius: 24px;
-  padding: 12px 36px;
-}
-
-.intro__info {
-  @include square(56px, 12px);
-  @include center-contents;
-
-  position: absolute;
-  right: 60px;
-  bottom: 100px;
-  color: $color-white;
-  background: $color-blue-500;
-  border: 2px solid $color-black;
-  font-size: 32px;
-}
-
-.intro__cta {
-  @include center-contents;
-
-  font-weight: $font-weight-medium;
-  font-size: $font-size-display-3;
-  border-radius: 1000px;
-  background: $color-white;
-  padding: 18px 18px 18px 36px;
-  line-height: 1;
-  border: 2px solid $color-black;
-  gap: 24px;
-}
-
-.intro__cta__arrow {
-  @include center-contents;
-
-  background: $color-blue-500;
-  height: 72px;
-  width: 72px;
-  border: 2px solid $color-black;
-  border-radius: 1000px;
-  color: $color-white;
-}
-</style>
---
+++ src/components/IntroScreen.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { PhInfo, PhArrowRight } from '@phosphor-icons/vue'
+</script>
+
+<template>
+  <div class="intro">
+    <div class="intro__text">
+      <h1>
+        <span class="intro__text__hi">Hi</span>
+        <span class="intro__text__name">I’m Mohan.</span>
+      </h1>
+      <button class="intro__info">
+        <PhInfo />
+      </button>
+    </div>
+
+    <button class="intro__cta">
+      Let’s talk
+      <div class="intro__cta__arrow">
+        <PhArrowRight />
+      </div>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/styles/colors.scss';
+@use '@/styles/helpers.scss';
+@use '@/styles/type.scss';
+
+.intro {
+  @include helpers.center-contents;
+
+  flex-direction: column;
+  gap: 36px;
+  position: fixed;
+  inset: 0;
+}
+
+.intro__text {
+  position: relative;
+}
+
+.intro__text__hi {
+  @include helpers.circle(128px);
+
+  font-weight: type.$weight-medium;
+  font-size: type.$size-display-2;
+  background-color: colors.$orange-500;
+  border: 2px solid colors.$black;
+  display: block;
+  text-align: center;
+  line-height: 118px;
+  margin-bottom: -24px;
+  transform: rotate(-10deg);
+}
+
+.intro__text__name {
+  font-family: type.$family-mono;
+  font-weight: type.$weight-bold;
+  font-size: type.$size-display-1;
+  color: colors.$white;
+  background-color: colors.$black;
+  display: block;
+  text-align: center;
+  border-radius: 24px;
+  padding: 12px 36px;
+}
+
+.intro__info {
+  @include helpers.square(56px, 12px);
+  @include helpers.center-contents;
+
+  position: absolute;
+  right: 60px;
+  bottom: 100px;
+  color: colors.$white;
+  background: colors.$blue-500;
+  border: 2px solid colors.$black;
+  font-size: 32px;
+}
+
+.intro__cta {
+  @include helpers.center-contents;
+
+  font-weight: type.$weight-medium;
+  font-size: type.$size-display-3;
+  border-radius: 1000px;
+  background: colors.$white;
+  padding: 18px 18px 18px 36px;
+  line-height: 1;
+  border: 2px solid colors.$black;
+  gap: 24px;
+}
+
+.intro__cta__arrow {
+  @include helpers.center-contents;
+
+  background: colors.$blue-500;
+  height: 72px;
+  width: 72px;
+  border: 2px solid colors.$black;
+  border-radius: 1000px;
+  color: colors.$white;
+}
+</style>
