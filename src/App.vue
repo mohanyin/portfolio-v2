409761afea622fbb3434720bed60a86c4df5ccb7
Mohan Yin <mohanyin@gmail.com>
2023-12-26 15:26:51 -0800

    add intro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PhInfo, PhArrowRight } from '@phosphor-icons/vue'
+</script>
 
 <template>
   <header>mohan yin</header>
 
   <main>
     <div class="intro">
-      <h1 class="intro__text">
-        <span class="intro__text__hi">Hi</span>
-        <span class="intro__text__name">I’m Mohan.</span>
-      </h1>
-      <button class="intro__button">
+      <div class="intro__text">
+        <h1>
+          <span class="intro__text__hi">Hi</span>
+          <span class="intro__text__name">I’m Mohan.</span>
+        </h1>
+        <button class="intro__info">
+          <PhInfo />
+        </button>
+      </div>
+
+      <button class="intro__cta">
         Let’s talk
-        <div class="intro__button__arrow"></div>
+        <div class="intro__cta__arrow">
+          <PhArrowRight />
+        </div>
       </button>
     </div>
   </main>
@@ -19,17 +29,15 @@
 
 <style lang="scss" scoped>
 @import '@/styles/variables.scss';
+@import '@/styles/helpers.scss';
 
 .intro {
-  display: flex;
+  @include center-contents;
+
   flex-direction: column;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  align-items: center;
   gap: 36px;
   position: fixed;
-  top: 0;
+  inset: 0;
 }
 
 .intro__text {
@@ -37,14 +45,13 @@
 }
 
 .intro__text__hi {
+  @include circle(128px);
+
   font-weight: $font-weight-medium;
   font-size: $font-size-display-2;
   background-color: $color-orange-500;
   border: 2px solid $color-black;
   display: block;
-  height: 124px;
-  width: 124px;
-  border-radius: 50%;
   text-align: center;
   line-height: 118px;
   margin-bottom: -24px;
@@ -63,7 +70,22 @@
   padding: 12px 36px;
 }
 
-.intro__button {
+.intro__info {
+  @include square(56px, 12px);
+  @include center-contents;
+
+  position: absolute;
+  right: 60px;
+  bottom: 100px;
+  color: $color-white;
+  background: $color-blue-500;
+  border: 2px solid $color-black;
+  font-size: 32px;
+}
+
+.intro__cta {
+  @include center-contents;
+
   font-weight: $font-weight-medium;
   font-size: $font-size-display-3;
   border-radius: 1000px;
@@ -71,17 +93,17 @@
   padding: 18px 18px 18px 36px;
   line-height: 1;
   border: 2px solid $color-black;
-  display: flex;
-  align-items: center;
   gap: 24px;
 }
 
-.intro__button__arrow {
+.intro__cta__arrow {
+  @include center-contents;
+
   background: $color-blue-500;
   height: 72px;
   width: 72px;
   border: 2px solid $color-black;
   border-radius: 1000px;
-  display: inline-block;
+  color: $color-white;
 }
 </style>
